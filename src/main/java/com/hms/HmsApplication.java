117c40d69c08e35ba4d9a13c15671b4efaf2f747
jaysonmulwa <jayson.mulwa@gmail.com>
2022-11-05 10:53:09 +0300

    fix: bug fix

--- src/main/java/com/hms/HmsApplication.java
+++ src/main/java/com/hms/HmsApplication.java
@@ -3,7 +3,8 @@ package com.hms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={
+		"com.hms.address", "com.hms.appointment", "com.hms.department", "com.hms.doctor", "com.hms.history", "com.hms.nurse", "com.hms.patient", "com.hms.payment"})
 public class HmsApplication {
 
 	public static void main(String[] args) {
