b9f20988f27df0fea5d0f4d29a04850a751f8fcf
jaysonmulwa <jayson.mulwa@gmail.com>
2022-12-11 17:52:24 +0300

    chore: more cors

--- src/main/java/com/hms/patient/PatientController.java
+++ src/main/java/com/hms/patient/PatientController.java
@@ -12,26 +12,31 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/patients")
     public List<Patient> getAllPatients() {
         return patientService.getAllPatients();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/patients/{id}")
     public Optional<Patient> getPatient(@PathVariable int id) {
         return patientService.getPatient(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.POST, value="/patients")
     public void addPatient(@RequestBody Patient patient) {
         patientService.addPatient(patient);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.PUT, value="/patients/{id}")
     public void updatePatient(@PathVariable int id, @RequestBody Patient patient) {
         patientService.updatePatient(id, patient);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.DELETE, value="/patients/{id}")
     public void deletePatient(@PathVariable int id) {
         patientService.deletePatient(id);
--- src/main/java/com/hms/payment/PaymentController.java
+++ src/main/java/com/hms/payment/PaymentController.java
@@ -12,26 +12,31 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/payment")
     public List<Payment> getAllPayments() {
         return paymentService.getAllPayments();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/payment/{id}")
     public Optional<Payment> getPayment(@PathVariable int id) {
         return paymentService.getPayment(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.POST, value="/payment")
     public void addPayment(@RequestBody Payment payment) {
         paymentService.addPayment(payment);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.PUT, value="/payment/{id}")
     public void updatePayment(@PathVariable int id, @RequestBody Payment payment) {
         paymentService.updatePayment(id, payment);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(method = RequestMethod.DELETE, value="/payment/{id}")
     public void deletePayment(@PathVariable int id) {
         paymentService.deletePayment(id);
